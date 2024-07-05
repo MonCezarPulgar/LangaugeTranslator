1ac5ab08509a3c4e085154cedb75180d55f6ac1e
MonCezarPulgar <pulgarmoncezar@gmail.com>
2024-07-05 14:00:25 +0800

    Add files via upload

---
+++ trial.php
@@ -0,0 +1,19 @@
+<html>
+	<head>
+		<title>PremTranslate:Languagen Translator</title>
+		<title>PremTranslate:Language Translator</title>
+  		<link rel="stylesheet" href="bootstrap-5.1.3/css/bootstrap.min.css">
+    	<script src="bootstrap-5.1.3/js/bootstrap.bundle.min.js"></script>
+    	<link rel="stylesheet" href="fontawesome-free-6.2.0-web/fontawesome-free-6.2.0-web/css/all.css">
+		<style>
+			h1{
+				text-align: center;
+			}
+		</style>
+	</head>
+	<body>
+		<div class = "container bg-white p-4">
+			<h1>Try out 2 Languages to Translate for FREE!</h1>
+		</div>
+	</body>
+</html>
